c84b1403940e9949a58fb215349bdd9410d0334f
lanc4r <lanc4r1996@gmail.com>
2019-09-20 11:09:47 +0800

    HL的工具类，持续更新鸭

---
+++ src/main/java/con/lanc4r/HLTools/App.java
@@ -0,0 +1,13 @@
+package con.lanc4r.HLTools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/con/lanc4r/HLTools/consts/MainConsts.java
@@ -0,0 +1,7 @@
+package con.lanc4r.HLTools.consts;
+
+public class MainConsts {
+
+    public static final String ZERO_STR = "0";
+
+}
